c0added0c6e627dbd01b1b922566ed7dcc6eb7a2
Jonas Du Bois <jonas.du.bois@outlook.com>
2024-11-02 13:59:08 +0100

    new project called Task Force

--- ProgServ2/Exercice2/dbManager/I_ApiCRUD.php
+++ ProgServ2/Exercice2/dbManager/I_ApiCRUD.php
@@ -8,4 +8,8 @@ interface I_ApiCRUD {
     public function rendPersonnes(string $nom): array;
     public function modifiePersonne(int $id, Users $users): bool;
     public function supprimePersonne(int $id) : bool;
+    public function verifierIdentifiants(string $email, string $motDePasse): string;
+    public function compterNbUsers(): int;
+    public function getUserByToken($token);
+    public function confirmeInscription(int $userId): bool;
 }
